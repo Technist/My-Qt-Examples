1301e0efb08aea8683c6e4cbf140fe92003e79b0
John <ljunjie@sekolahciputra.sch.id>
2019-05-27 13:00:01 +0700

    Update main.cpp

--- [widgets]--logging/main.cpp
+++ [widgets]--logging/main.cpp
@@ -15,24 +15,15 @@ int main(int argc, char *argv[])
     QPushButton pb("Click me!");
     QTextEdit te;
     
-    QObject::connect(&pb, &QPushButton::clicked, [&] {
-        pb.setEnabled(false);
-        pb.setText("Logging...");
-        
+    QObject::connect(&pb, &QPushButton::clicked, [&te, &i] {
         double t = 0.0;
         for (int N = 1; N < 50; N++)
         {
             t += 2.0;
             te.append(QString("\tN=%1\tt=%2").arg(N).arg(t));
-            
-            //  for demonstration purposes only, don't use in production code
-            pb.setText("Logging" + QString().fill('.', (N/10)%3 + 1));
-            QThread::msleep(100);
-            QApplication::processEvents();
         }
         
         pb.setText("Click me!");
-        pb.setEnabled(true);
     });
     
     vbLayout.addWidget(&te);
